167e844e560d72895a049520f763a0f04b1a6bd4
Taka <amano.takahisa@gmail.com>
2025-09-21 22:54:10 +0200

    add treesitter

---
+++ modules/home-manager/editor/nixvim/plugins/treesitter.nix
@@ -0,0 +1,37 @@
+{ ... }:
+
+{
+  programs.nixvim = {
+    plugins.treesitter = {
+      enable = true;
+      settings = {
+        highlight = {
+          enable = true;
+        };
+        indent = {
+          enable = true;
+        };
+        ensure_installed = [
+          "bash"
+            "c"
+            "html"
+            "javascript"
+            "json"
+            "lua"
+            "markdown"
+            "nix"
+            "python"
+            "query"
+            "regex"
+            "rust"
+            "tsx"
+            "typescript"
+            "vim"
+            "vimdoc"
+            "yaml"
+        ];
+        auto_install = true;
+      };
+    };
+  };
+}
