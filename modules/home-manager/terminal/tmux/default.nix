ca43fe3eb9b723aa79d9f935ecba5166cbd89a88
Taka <amano.takahisa@gmail.com>
2025-09-21 17:00:17 +0200

    update tmux settings

--- modules/home-manager/terminal/tmux/default.nix
+++ modules/home-manager/terminal/tmux/default.nix
@@ -8,6 +8,27 @@
     mouse = true;
     keyMode = "vi";
     extraConfig = ''
+      # Keep current working directory when open new window or panes
+      bind c new-window -c "#{pane_current_path}"
+      bind % split-window -hc "#{pane_current_path}"
+      bind '"' split-window -vc "#{pane_current_path}"
+
+      #################
+      # Neovim compatibility
+      #################
+      # Fix escape-time for Neovim
+      set-option -sg escape-time 10
+
+      # Enable focus-events for autoread
+      set-option -g focus-events on
+
+      # Set proper terminal for colors
+      set-option -g default-terminal "screen-256color"
+
+      # Enable true color support
+      set-option -sa terminal-features ',xterm-256color:RGB'
+      set-option -sa terminal-overrides ',xterm-256color:Tc'
+
       #################
       # Pains
       #################
@@ -23,16 +44,16 @@
       bind -n M-Down select-pane -D
 
       # Change pane border colors
-      set -g pane-active-border-style "bg=#ffffa4,fg=#000000"
+      set-option -g pane-active-border-style "bg=#ffffa4,fg=#000000"
 
       # Keep showing window numbe when <prefix> q
       bind -T prefix e display-panes -d 0
 
       # keep pane title static
-      set -g automatic-rename off
-      set -g pane-border-format "#{pane_index}:#T #{pane_current_path}"
-      set -g status-interval 5
-      set -g pane-border-status bottom
+      set-option -g automatic-rename off
+      set-option -g pane-border-format "#{pane_index}:#T #{pane_current_path}"
+      set-option -g status-interval 5
+      set-option -g pane-border-status bottom
     '';
 
 
