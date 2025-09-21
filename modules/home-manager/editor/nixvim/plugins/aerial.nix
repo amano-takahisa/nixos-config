8c546ace474cb36d76d0b5d6b3a0423ee3253c90
Taka <amano.takahisa@gmail.com>
2025-09-21 20:18:19 +0200

    tweak aerial float position

--- modules/home-manager/editor/nixvim/plugins/aerial.nix
+++ modules/home-manager/editor/nixvim/plugins/aerial.nix
@@ -4,6 +4,26 @@
   programs.nixvim = {
     plugins.aerial = {
       enable = true;
+      settings = {
+        # set floating window position at top right
+        float = {
+          relative = "win";
+          override = {
+            __raw = ''
+              function(conf, source_winid)
+                -- This is the config that will be passed to nvim_open_win.
+                -- Change values here to customize the layout
+                local padding = 1
+                conf.zindex = 25
+                conf.anchor = "NE"
+                conf.row = padding
+                conf.col = vim.api.nvim_win_get_width(source_winid) - padding
+                return conf
+              end
+            '';
+          };
+        };
+      };
     };
     keymaps = [
     {
