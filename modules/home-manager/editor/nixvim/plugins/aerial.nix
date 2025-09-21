cfd33d65d9b0b73c7563ed0ca601b840215ea774
Taka <amano.takahisa@gmail.com>
2025-09-21 20:05:14 +0200

    add aerial keymap

--- modules/home-manager/editor/nixvim/plugins/aerial.nix
+++ modules/home-manager/editor/nixvim/plugins/aerial.nix
@@ -5,6 +5,26 @@
     plugins.aerial = {
       enable = true;
     };
+    keymaps = [
+    {
+      mode = "n";
+      key = "<leader>a";
+      action = ":AerialToggle! float<CR>";
+      options = {
+        silent = true;
+        desc = "Toggle Aerial";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>an" ;
+      action = ":AerialNavToggle<CR>";
+      options = {
+        silent = true;
+        desc = "Toggle Aerial Navigation";
+      };
+    }
+    ];
   };
 }
 
