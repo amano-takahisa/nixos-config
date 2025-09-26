d0164b2ef1992ec550acc2ab5f63f19a0e345b15
Taka <amano.takahisa@gmail.com>
2025-09-26 08:42:00 +0200

    update system packages

--- modules/home-manager/common/default.nix
+++ modules/home-manager/common/default.nix
@@ -1,20 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   # Common settings for all environments
   home = {
     username = "takahisa";
     homeDirectory = "/home/takahisa";
-
-    # Basic packages for all environments
-    packages = with pkgs; [
-      # System utilities
-      # curl
-      # htop
-      tree
-      # unzip
-      # wget
-    ];
   };
 
   # Enable home-manager
--- modules/system/packages/development.nix
+++ modules/system/packages/development.nix
@@ -1,12 +1,17 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
+    # packages for development, for all users
+    bat
     clang
     gcc
     gnumake
+    htop
+    jq
     nodejs
     pkg-config
+    tree
     tree-sitter
     wl-clipboard
   ];
