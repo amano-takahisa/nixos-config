34284eb5ac20c817032b9481575f69f10ce50a34
Taka <amano.takahisa@gmail.com>
2025-09-28 21:21:17 +0200

    config for atuin

--- modules/home-manager/cli-tools/atuin/default.nix
+++ modules/home-manager/cli-tools/atuin/default.nix
@@ -5,6 +5,26 @@
     enable = true;
     enableFishIntegration = true;
     daemon.enable = true;
+    settings = {
+      theme.name = "catppuccin-mocha-blue";
+      style = "auto";
+      invert = false;
+    };
+    themes = {
+      "catppuccin-mocha-blue" = {
+        theme.name = "catppuccin-mocha-blue";
+        colors = {
+          AlertInfo = "#a6e3a1";
+          AlertWarn = "#fab387";
+          AlertError = "#f38ba8";
+          Annotation = "#89b4fa";
+          Base = "#cdd6f4";
+          Guidance = "#9399b2";
+          Important = "#f38ba8";
+          Title = "#89b4fa";
+        };
+      };
+    };
   };
 }
 
