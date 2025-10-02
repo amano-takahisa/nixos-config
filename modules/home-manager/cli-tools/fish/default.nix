6ec9d5591d2b746788b28882365e3a6ae850952f
Taka <amano.takahisa@gmail.com>
2025-10-02 18:21:42 +0200

    custom fish prompt

--- modules/home-manager/cli-tools/fish/default.nix
+++ modules/home-manager/cli-tools/fish/default.nix
@@ -19,7 +19,8 @@
     };
     interactiveShellInit = ''
       set -g fish_prompt_pwd_dir_length 2
-      set -g fish_prompt_pwd_full_dirs 2
+      set -g fish_prompt_pwd_full_dirs 3
+      source ${./fish_prompt.fish}
     '';
   };
 }
