9d1c020bcd47e58a5a965e23fd422e692ff00f8a
Taka <amano.takahisa@gmail.com>
2025-10-05 13:16:07 +0200

    keep indent blankline active in visual mode

--- modules/home-manager/editor/nixvim/colorschemes/tokyonight.nix
+++ modules/home-manager/editor/nixvim/colorschemes/tokyonight.nix
@@ -5,9 +5,17 @@
     colorschemes.tokyonight = {
       enable = true;
       settings = {
-        style = "night";
+        style = "night"; # Options: storm, night, day, moon
         transparent = false;
-        terminal_colors = true;
+        terminal_colors = false;
+        on_highlights = ''
+          -- TODO: Change highlight colors based on styles.
+          --       Currently the followings are applied to all styles.
+          function(highlights, colors)
+            highlights.CursorColumn = { bg = "#1F2433" }
+            highlights.CursorLine = { bg = "#1F2433" }
+          end
+        '';
       };
     };
   };
--- modules/home-manager/editor/nixvim/plugins/indent-blankline.nix
+++ modules/home-manager/editor/nixvim/plugins/indent-blankline.nix
@@ -45,10 +45,6 @@
           remove_blankline_trail = false;
           highlight = [ "CursorColumn" "WhiteSpace" ];
         };
-        # "#3b4261"
-        # "#2B3147"
-        # "#1F2433"
-        # "#1D2230"
         scope = {
           enabled = true;
           show_start = true;
@@ -66,22 +62,6 @@
         };
       };
     };
-    extraConfigLua = ''
-      local indent_blankline_augroup = vim.api.nvim_create_augroup("indent_blankline_augroup", { clear = true })
-        vim.api.nvim_create_autocmd("ModeChanged", {
-          group = indent_blankline_augroup,
-          pattern = "[vV\x16]*:*",
-          command = "IBLEnable",
-          desc = "Enable indent-blankline when exiting visual mode",
-        })
-
-        vim.api.nvim_create_autocmd("ModeChanged", {
-          group = indent_blankline_augroup,
-          pattern = "*:[vV\x16]*",
-          command = "IBLDisable",
-          desc = "Disable indent-blankline when exiting visual mode",
-        })
-    '';
   };
 }
 
