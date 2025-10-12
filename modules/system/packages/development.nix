db59015f70bd33da04af3de77abf12d11577c183
Taka <amano.takahisa@gmail.com>
2025-10-12 11:19:09 +0200

    comments

--- modules/system/packages/development.nix
+++ modules/system/packages/development.nix
@@ -3,17 +3,17 @@
 {
   environment.systemPackages = with pkgs; [
     # packages for development, for all users
-    bat
-    clang
-    gcc
-    gnumake
-    htop
-    jq
-    nodejs
-    pkg-config
-    tree
-    tree-sitter
-    unzip
-    wl-clipboard
+    bat # A cat(1) clone with syntax highlighting and Git integration.
+    clang # C language family frontend for LLVM
+    gcc # GNU Compiler Collection
+    gnumake # GNU make utility to maintain groups of programs
+    htop # An interactive process viewer for Unix systems
+    jq # Command-line JSON processor
+    nodejs # Evented I/O for V8 javascript
+    pkg-config # Tool that allows packages to find out information about other packages (wrapper script)
+    tree # Command to produce a depth indented directory listing
+    tree-sitter # Parser generator tool and an incremental parsing library
+    unzip # Extraction utility for archives compressed in .zip format
+    wl-clipboard # Command-line copy/paste utilities for Wayland
   ];
 }
