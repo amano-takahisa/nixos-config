675453b8b6f44b4495da740d9aa2f532816f37de
Taka <amano.takahisa@gmail.com>
2025-10-12 17:24:31 +0200

    update configration
    sounds, comments

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -1,8 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
+# Edit this configuration file to define what should be installed on your
+# system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports =
@@ -19,6 +19,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 20;
   boot.loader.efi.canTouchEfiVariables = true;
+
   # Use latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
@@ -63,8 +64,10 @@
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "";
+  # };
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -74,7 +77,20 @@
   # OR
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    # alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+
+    # Fix sound wakeup issue
+    extraConfig.pipewire."context.properties" = {
+      "suspend.timeout-seconds" = 0;
+    };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -83,7 +99,7 @@
   # Enable fish shell
   programs.fish.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.takahisa = {
     isNormalUser = true;
     description = "takahisa";
@@ -93,7 +109,8 @@
     ];
   };
 
-  # programs.firefox.enable = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
@@ -101,6 +118,7 @@
   #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #   wget
   # ];
+
   # This settings enables Ozone/Wayland support for Electron apps like
   # Slack and Discord.
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
