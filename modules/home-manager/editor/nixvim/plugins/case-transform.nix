9a03fc186263cb655fce00f074d4c991fe583fbc
Taka <amano.takahisa@gmail.com>
2025-10-13 07:21:12 +0200

    remove directory check

--- modules/home-manager/editor/nixvim/plugins/case-transform.nix
+++ modules/home-manager/editor/nixvim/plugins/case-transform.nix
@@ -1,29 +1,19 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
-let
-  pluginPath = /home/takahisa/ghq/personal/github.com/amano-takahisa/case-transform.nvim;
-  pluginExists = builtins.pathExists pluginPath;
-in
 {
   programs.nixvim = {
-    # Load the local plugin for development if it exists
-    extraPlugins = lib.optionals pluginExists [
-      (pkgs.vimUtils.buildVimPlugin rec {
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
         pname = "case-transform.nvim";
         version = "dev";
-        src = pluginPath;
-        # Remove empty doc directory to avoid helptags generation error
-        postPatch = ''
-          rm -rf doc
-        '';
+        src = /home/takahisa/ghq/personal/github.com/amano-takahisa/case-transform.nvim;
+        postPatch = "rm -rf doc";
         meta.homepage = "https://github.com/amano-takahisa/case-transform.nvim";
       })
     ];
 
-    extraConfigLua = lib.optionalString pluginExists ''
-      require('case-transform').setup({
-        default_keymaps = true,
-      })
+    extraConfigLua = ''
+      require('case-transform').setup({ default_keymaps = true })
     '';
   };
 }
