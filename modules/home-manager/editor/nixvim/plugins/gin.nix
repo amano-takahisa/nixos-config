4046a255f80d31f45ae55165cb1c86edd6c6b8aa
Taka <amano.takahisa@gmail.com>
2025-10-19 20:51:36 +0200

    disable gin

--- modules/home-manager/editor/nixvim/plugins/gin.nix
+++ modules/home-manager/editor/nixvim/plugins/gin.nix
@@ -1,22 +1,24 @@
+# disable this plugin
+
 { pkgs, ... }:
 
 {
-  programs.nixvim = {
-    extraPlugins = [
-      (pkgs.vimUtils.buildVimPlugin {
-        name = "vim-gin";
-        src = pkgs.fetchFromGitHub {
-          owner = "lambdalisue";
-          repo = "vim-gin";
-          rev = "353e32d6e37228c0f51a6cf72c06210bb1340af9";
-          hash = "sha256-zlNdfm+oUwcvzExFvtTqnHnwE8uqdOlxeUOGmVOBwIg=";
-        };
-      })
-    ];
+  # programs.nixvim = {
+  #   extraPlugins = [
+  #     (pkgs.vimUtils.buildVimPlugin {
+  #       name = "vim-gin";
+  #       src = pkgs.fetchFromGitHub {
+  #         owner = "lambdalisue";
+  #         repo = "vim-gin";
+  #         rev = "353e32d6e37228c0f51a6cf72c06210bb1340af9";
+  #         hash = "sha256-zlNdfm+oUwcvzExFvtTqnHnwE8uqdOlxeUOGmVOBwIg=";
+  #       };
+  #     })
+  #   ];
 
-    extraConfigVim = ''
-      source ${./plugins-extra/gin-preview.vim}
-    '';
-  };
+  #   extraConfigVim = ''
+  #     source ${./plugins-extra/gin-preview.vim}
+  #   '';
+  # };
 }
 
