2ef8df1db9fd5e66d6f390bc940b733b5f4d303c
Taka <amano.takahisa@gmail.com>
2025-10-22 11:32:03 +0200

    fix memory issue while buildin

--- rebuild.sh
+++ rebuild.sh
@@ -13,6 +13,15 @@ OPERATION=${2:-switch}
 echo "Run: sudo nixos-rebuild $OPERATION --flake .#$HOST --impure"
 
 export NIXPKGS_ALLOW_UNFREE=1
+
+# to resolve memory issues during build wsl
+# if hostname is 'wsl', limit the number of parallel jobs
+if [ "$HOST" == "wsl" ]; then
+  export NIX_MAX_JOBS=2
+  export NIX_BUILD_CORES=2
+  export CARGO_BUILD_JOBS=2
+fi
+
 sudo -E nixos-rebuild $OPERATION --flake .#$HOST --impure
 
 echo "Build complete!"
