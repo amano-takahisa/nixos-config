c5dd9a68eb1a5051d24f5ff02d79da3f420c5cc9
Taka <amano.takahisa@gmail.com>
2025-10-23 14:45:07 +0200

    add spotdiff

--- modules/home-manager/editor/nixvim/plugins/neo-tree.nix
+++ modules/home-manager/editor/nixvim/plugins/neo-tree.nix
@@ -6,61 +6,32 @@
 
     plugins.neo-tree = {
       enable = true;
-      settings = {
-        enable_git_status = false;
-        filesystem = {
-          filtered_items = {
-            hide_dotfiles = false;
-          };
-          use_libuv_file_watcher = true;
-        };
-        default_component_configs.symlink_target.enabled = true;
-        window = {
-          width = 40;
-          mappings = {
-            "<space>" = "toggle_node";
-            "<cr>" = "open";
-            "<esc>" = "cancel";
-            "l" = "open";
-            "h" = "close_node";
-            "S" = "open_split";
-            "s" = "open_vsplit";
-            "t" = "open_tabnew";
-            "C" = "close_node";
-            "z" = "close_all_nodes";
-            "a" = "add";
-            "A" = "add_directory";
-            "d" = "delete";
-            "r" = "rename";
-            "y" = "copy_to_clipboard";
-            "x" = "cut_to_clipboard";
-            "p" = "paste_from_clipboard";
-            "c" = "copy";
-            "m" = "move";
-            "q" = "close_window";
-            "R" = "refresh";
-            "?" = "show_help";
-          };
-        };
-      };
     };
     extraConfigLuaPost = ''
+      local neotree_config = {
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+          },
+          use_libuv_file_watcher = true,
+        },
+        default_component_configs = {
+          symlink_target = {
+            enabled = true,
+          },
+        },
+      }
+
       -- show git status only in ghq directories
       local function update_neotree_git_status()
         local cwd = vim.fn.getcwd()
         local ghq_path = vim.fn.expand("~/ghq")
-
-        if cwd:match("^" .. vim.pesc(ghq_path)) then
-          require("neo-tree").setup({
-            enable_git_status = true
-          })
-        else
-          require("neo-tree").setup({
-            enable_git_status = false
-          })
-        end
+        local config = vim.deepcopy(neotree_config)
+        config.enable_git_status = vim.startswith(cwd, ghq_path)
+        require("neo-tree").setup(config)
       end
 
+      -- Initial setup
       update_neotree_git_status()
 
       vim.api.nvim_create_autocmd("DirChanged", {
---
+++ modules/home-manager/editor/nixvim/plugins/spotdiff.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  programs.nixvim = {
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "spotdiff.vim";
+        src = pkgs.fetchFromGitHub {
+          owner = "rickhowe";
+          repo = "spotdiff.vim";
+          rev = "4f7a60fe4fd6ba5674d91741dd8dddd5a73e419a";
+          hash = "sha256-y6O0uaMY7PbGp6SknmVClh2s5BrKpNxd4wjDRxDyYTo=";
+        };
+      })
+    ];
+  };
+}
