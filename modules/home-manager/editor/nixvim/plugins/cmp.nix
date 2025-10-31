a1069cfb831eade63219c2e31d83542e7db27188
Taka <amano.takahisa@gmail.com>
2025-10-31 13:11:12 +0100

    disable autocomplete on nvim

--- modules/home-manager/editor/nixvim/plugins/cmp.nix
+++ modules/home-manager/editor/nixvim/plugins/cmp.nix
@@ -11,6 +11,10 @@
           expand = "function(args) require('luasnip').lsp_expand(args.body) end";
         };
 
+        completion = {
+          autocomplete = false;
+        };
+
         mapping = {
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
