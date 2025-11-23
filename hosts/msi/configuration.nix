a040ec4e40b6d582f9cf6925b3f2be4c48cd9e1a
Taka <amano.takahisa@gmail.com>
2025-11-23 07:34:40 +0100

    add cuda

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -147,13 +147,13 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.cudaSupport = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    cudatoolkit
+  ];
 
   # This settings enables Ozone/Wayland support for Electron apps like
   # Slack and Discord.
