320917657af4788fcf9e11b0f5adc4cc8f02bb42
Taka <amano.takahisa@gmail.com>
2026-01-14 17:14:19 +0100

    add gwq

---
+++ modules/home-manager/vcs/gwq/default.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+let
+  gwq = pkgs.buildGoModule rec {
+    pname = "gwq";
+    version = "0.0.7";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "d-kuro";
+      repo = "gwq";
+      rev = "v${version}";
+      hash = "sha256-CvfAxTd7/AK98TSJDM+iNJTUALMKMk8esXEn7Fuumik=";
+    };
+
+    vendorHash = "sha256-c1vq9yETUYfY2BoXSEmRZj/Ceetu0NkIoVCM3wYy5iY=";
+
+    meta = with pkgs.lib; {
+      description = "Git worktree manager with fuzzy finder";
+      homepage = "https://github.com/d-kuro/gwq";
+      license = licenses.mit;
+    };
+  };
+in
+{
+  home.packages = [ gwq ];
+}
