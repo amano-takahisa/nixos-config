07c6ec027b8dd678f06665c8f14e818902ace11f
Taka <amano.takahisa@gmail.com>
2026-02-12 17:33:18 +0100

    update

--- modules/home-manager/cli-tools/gitstats/default.nix
+++ modules/home-manager/cli-tools/gitstats/default.nix
@@ -12,28 +12,65 @@
     display_depth = 2  # trailing path segments shown in Name column
 
     [table.columns.last_commit]
-    command = "git log -1 --format=%cs"
     width   = 10
     title   = "Commit"
-    type    = "str"
+    type    = "datetime"
+    format  = "YYYY-MM-DD"
+    timeout = "2s"
+
+    [table.columns.last_fs]
+    width   = 10
+    type    = "datetime"
+    format  = "YYYY-MM-DD"
     timeout = "2s"
 
     [table.columns.untracked]
-    command = "git ls-files --others --exclude-standard | wc -l"
+    # command = "git ls-files --others --exclude-standard | wc -l"
     title   = "??"
-    width   = 5
+    width   = 2
+    type    = "int"
+
+    [table.columns.unpushed_br]
+    title   = ""
+    width   = 2
+    type    = "int"
+
+    [table.columns.unpushed_cm]
+    title   = ""
+    width   = 2
+    type    = "int"
+
+    [table.columns.uncommitted]
+    command = "git diff --name-only HEAD | wc -l"
+    title   = "MM"
+    width   = 2
+    type    = "int"
+
+    [table.columns.stash]
+    command = "git rev-list --walk-reflogs --count refs/stash 2>/dev/null || echo 0"
+    title   = ""
+    width   = 2
     type    = "int"
 
     [table.columns.tracking]
     command = "git ls-files --exclude-standard | wc -l"
-    title   = "ls-files"
+    title   = "ls"
     type    = "int"       # string|int|size|duration (controls sorting/format)
-    width   = 8           # optional column width
+    width   = 5           # optional column width
     timeout = "2s"        # optional per-command timeout (falls back to CLI timeout)
     # env    = { FOO = "bar" } # optional extra env vars
 
     [detail.commands.default]
-    command = "git status --short && echo --- stash --- && git stash list && echo ------------ && ls -lha"
+    command = """
+    echo $ git status && \
+    git status --short && \
+    echo ------------ && \
+    echo $ git stash list && \
+    git stash list && \
+    echo ------------ && \
+    echo $ ls -lha && \
+    ls -lha
+    """
     title   = "Status"
   '';
 }
