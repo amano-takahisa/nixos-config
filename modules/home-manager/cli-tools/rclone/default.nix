00b31154e20b3278ea1810b578f2a980c1dcccf9
Taka <amano.takahisa@gmail.com>
2026-02-19 01:21:10 +0100

    rclone

--- modules/home-manager/cli-tools/rclone/default.nix
+++ modules/home-manager/cli-tools/rclone/default.nix
@@ -1,7 +1,25 @@
-{ pkgs, ... }:
+{ config, ... }:
 
 {
-  home.packages = with pkgs; [
-    rclone
-  ];
+  programs.rclone = {
+    enable = true;
+  };
+
+  sops.secrets = {
+    rclone_r2_access_key_id = { };
+    rclone_r2_secret_access_key = { };
+    rclone_r2_endpoint = { };
+  };
+
+  sops.templates."rclone.conf" = {
+    content = ''
+      [r2]
+      type = s3
+      provider = Cloudflare
+      access_key_id = ${config.sops.placeholder.rclone_r2_access_key_id}
+      secret_access_key = ${config.sops.placeholder.rclone_r2_secret_access_key}
+      endpoint = ${config.sops.placeholder.rclone_r2_endpoint}
+    '';
+    path = "${config.home.homeDirectory}/.config/rclone/rclone.conf";
+  };
 }
