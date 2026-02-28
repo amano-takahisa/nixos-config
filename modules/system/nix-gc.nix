72846925ec2e4551b6e6446d2887d2582e3918bb
Taka <amano.takahisa@gmail.com>
2026-02-28 15:01:31 +0100

    nix optimization

--- modules/system/nix-gc.nix
+++ modules/system/nix-gc.nix
@@ -9,6 +9,10 @@
     options = "--delete-older-than 30d";
   };
 
+  # Automatic Nix store optimization
+  # Replaces duplicate files in the store with hard links to save disk space
+  nix.optimise.automatic = true;
+
   # Prune old system generations during activation
   system.activationScripts.prune-old-generations.text = ''
     ${config.nix.package}/bin/nix-env --profile /nix/var/nix/profiles/system --delete-generations +10
