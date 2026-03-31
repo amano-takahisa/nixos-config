30408ff1ee829d15c30557faef98de75c291a287
Taka <amano.takahisa@gmail.com>
2026-04-01 08:16:07 +0900

    default app bitwarden

--- modules/home-manager/security/bitwarden/default.nix
+++ modules/home-manager/security/bitwarden/default.nix
@@ -4,5 +4,12 @@
   home.packages = with pkgs; [
     bitwarden-desktop
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/bitwarden" = "bitwarden.desktop";
+    };
+  };
 }
 
