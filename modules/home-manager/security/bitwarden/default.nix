a019fce07dfca171f70af91c8e9deff10b8de894
Taka <amano.takahisa@gmail.com>
2026-06-02 10:20:06 +0900

    remove duplicated

--- modules/home-manager/security/bitwarden/default.nix
+++ modules/home-manager/security/bitwarden/default.nix
@@ -8,7 +8,7 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "x-scheme-handler/bitwarden" = [ "bitwarden.desktop" "Bitwarden.desktop" ];
+      "x-scheme-handler/bitwarden" = "bitwarden.desktop";
     };
   };
 }
