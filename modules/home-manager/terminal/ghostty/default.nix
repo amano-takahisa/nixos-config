dabf4517ad088e5f0db3f7f112599cf845d801f1
Taka <amano.takahisa@gmail.com>
2026-06-20 07:29:49 +0900

    update ghostty config

--- modules/home-manager/terminal/ghostty/default.nix
+++ modules/home-manager/terminal/ghostty/default.nix
@@ -4,6 +4,11 @@
   programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      # ctrl + shift + , to reload ghostty settings.
+      # https://ghostty.org/docs/config/reference
+      adjust-cursor-thickness = 6;
+    };
   };
 }
 
