4a31ac398445a976c4e82969c40db51bb7555749
Taka <amano.takahisa@gmail.com>
2026-07-01 20:06:35 +0900

    disable ligature

--- modules/home-manager/terminal/ghostty/default.nix
+++ modules/home-manager/terminal/ghostty/default.nix
@@ -8,6 +8,11 @@
       # ctrl + shift + , to reload ghostty settings.
       # https://ghostty.org/docs/config/reference
       adjust-cursor-thickness = 6;
+      font-feature = [
+        "-calt"
+        "-liga"
+        "-dlig"
+      ];
     };
   };
 }
