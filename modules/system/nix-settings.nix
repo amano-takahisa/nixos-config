99bf8ade1f9ae9b42458ba585cf1d088f39bd9f1
Taka <amano.takahisa@gmail.com>
2026-09-24 09:56:26 +0200

    drop 404 caches

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -18,7 +18,6 @@
     substituters = [
       "https://cache.nixos.org" # Official NixOS cache
       "https://nix-community.cachix.org" # Community packages (nixvim, plasma-manager, home-manager)
-      "https://cuda-maintainers.cachix.org" # CUDA-enabled packages (critical for GPU computing)
       "https://numtide.cachix.org" # Development tools (devenv, treefmt)
     ];
 
@@ -26,7 +25,6 @@
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     ];
 
