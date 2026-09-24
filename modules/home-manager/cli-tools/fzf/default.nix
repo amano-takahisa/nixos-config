b7fb025973d6c4fcf24a267b026eb77091d86eb4
Taka <amano.takahisa@gmail.com>
2026-09-24 10:11:29 +0200

    disable ctrl-r of fzf

--- modules/home-manager/cli-tools/fzf/default.nix
+++ modules/home-manager/cli-tools/fzf/default.nix
@@ -4,6 +4,11 @@
   programs.fzf = {
     enable = true;
     enableFishIntegration = true;
+
+    # Atuin (sourced after fzf) owns Ctrl-R for fish history search.
+    # Empty string = the supported way to yield Ctrl-R to the history manager;
+    # silences the home-manager fzf/atuin Ctrl-R conflict warning.
+    historyWidget.command = "";
   };
 }
 
